52e708ab979ba6c02276b252d54777b07d612d90
Fandaskala <110682455+Fandaskala@users.noreply.github.com>
2024-09-01 18:16:45 +0200

    color
    add color

--- wifi_check.py
+++ wifi_check.py
@@ -1,8 +1,9 @@
 from json import dump, load
-from time import strftime, localtime
+from time import strftime, localtime, sleep
+from colorama import Fore
 import requests
 print ('wifi connection tester started\n you can find log.json for successful connection\n process started: \n\n\n')
-def file_write_log(str: str):
+def file_write_log(str: str) -> None:
     timer = strftime("%Y-%m-%d %H:%M:%S", localtime())
     try:
         with open('log.json', "r", newline=None) as log_read:
@@ -18,7 +19,8 @@ while True:
         # Replace "https://www.google.com/" with a preferred website
         response = requests.get("https://www.google.com/", timeout=5)  # Set a timeout
         response.raise_for_status()  # Raise exception for non-2xx status codes
-        print(timer, " connected to wifi")
+        print(timer, Fore.GREEN, " connected to wifi", Fore.WHITE)
         file_write_log(" connected to wifi")
+        sleep(15)  # Wait 15 seconds before checking again
     except (requests.exceptions.RequestException, TimeoutError):
-        print(timer," Timeout conection failed")
+        print(timer ,Fore.RED ," Timeout conection failed", Fore.WHITE)
